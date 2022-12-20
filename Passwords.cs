52546e6f88a37b316203520913fa04b01261237d
hacktheegg <99865348+hacktheegg@users.noreply.github.com>
2022-12-20 22:15:58 +1100

    committing but also not committing

---
+++ Passwords.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramSelector
+{
+    public class Passwords
+    {
+        public static string[] userPasswords =
+        {
+            "user1",
+            "user2",
+            "user3"
+        };
+    }
+}
